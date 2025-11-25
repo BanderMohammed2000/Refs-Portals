930817a1740645bae9d043d12ca443d93fe7d4d5
Bander Btati <bander.mohammed.2000@gmail.com>
2025-11-25 22:16:23 +0300

    the project is done

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,44 @@
+// ملاحظة: نحن هنا نستخدم النهج القديم الذي في الاصدارات القديمة من رياكت لقبول ref من مكون الاب
+// في الاصدارات الجديدة لانحتاج استخدام forwardRef
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const ResultModal = forwardRef(function ResultModel(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  // لا تستخدم useImperativeHandle إلا عند الضرورة القصوى.
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
+
+export default ResultModal;
